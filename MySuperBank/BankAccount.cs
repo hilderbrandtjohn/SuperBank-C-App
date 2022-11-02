b82e9f7c586de499cf9962adac96fdb26f2091d9
hilderbrandtjohn <hilderbrandtjohn5@gmail.com>
2022-11-02 06:02:01 +0300

    withdraw and deposit functions and missing return value to get balance

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -11,14 +11,15 @@ namespace MySuperBank
         public string Number { get; }
         public string Owner { get; set; }
 
-        public decimal Balance { get 
-            {
+        public decimal Balance { 
+            get{
                 decimal balance = 0;
                 foreach(var item in AllTransactions)
                 {
                     balance = +item.Amount;
 
                 }
+                return balance;
             }
         }
 
@@ -28,18 +29,36 @@ namespace MySuperBank
 
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;  
-            this.Balance = initialBalance;
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "initial deposit");
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, String note) 
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+
+            }
+            var deposit = new Transactions(amount, date, note); 
+            AllTransactions.Add(deposit);   
         }
         public void MakeWithdrawal(decimal amount, DateTime date, String note)
         {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdarawal must be positive");
+                
+            }
+            if (Balance-amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
 
+            }
+            var withdrawal = new Transactions(-amount, date, note);
+            AllTransactions.Add(withdrawal);
         }
     }
 }
